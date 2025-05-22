17ac3afe625ab5c134090a701b69485522bbeca9
ybt <1011265301@qq.com>
2025-05-22 12:26:42 +0800

    22301106-实验4

--- user/target/riscv64gc-unknown-none-elf/release/deps/03my_application-d7af106d1b376827.d
+++
@@ -1,5 +0,0 @@
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03my_application-d7af106d1b376827: src/bin/03my_application.rs
-
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03my_application-d7af106d1b376827.d: src/bin/03my_application.rs
-
-src/bin/03my_application.rs:
