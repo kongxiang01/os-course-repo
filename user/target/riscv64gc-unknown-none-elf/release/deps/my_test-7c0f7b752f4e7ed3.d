6f8b3a75130222592aeaa043c8dfccee4d84f79a
ybt <1011265301@qq.com>
2025-06-15 16:43:14 +0800

    22301106-实验7提交

--- user/target/riscv64gc-unknown-none-elf/release/deps/my_test-7c0f7b752f4e7ed3.d
+++
@@ -1,5 +0,0 @@
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/my_test-7c0f7b752f4e7ed3: src/bin/my_test.rs
-
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/my_test-7c0f7b752f4e7ed3.d: src/bin/my_test.rs
-
-src/bin/my_test.rs:
--- user/target/riscv64gc-unknown-none-elf/release/my_test.d
+++
@@ -1,1 +0,0 @@
-/mnt/user/target/riscv64gc-unknown-none-elf/release/my_test: /mnt/user/src/bin/my_test.rs /mnt/user/src/console.rs /mnt/user/src/lang_items.rs /mnt/user/src/lib.rs /mnt/user/src/syscall.rs
